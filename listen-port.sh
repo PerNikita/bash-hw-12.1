fde152d871b0c6dce2650c9ad97f481526c4fd92
nikita.perepechin <n.perepechins@gmail.com>
2026-04-16 13:26:19 +0300

    edit listen-port.sh

--- listen-port.sh
+++ listen-port.sh
@@ -6,13 +6,21 @@ read -p "Введите удаленный хост или нажмите Enter 
 
 if [ -n "$HOST" ]; then
 	for port in {80..8000}; do
-		$(nc -zv $HOST $port | grep succeeded)
+			
+		if $(nc -z $HOST $port &> /dev/null); then
+			echo "Port $port busy"
+		else
+			echo "Port $port free"
+		fi
+
 	done
 else
 	for port in {80..8000}; do
 		for local_port in $ALL_PORTS; do
 			if [[ "$port" == "$local_port" ]]; then
-				echo $port;
+				echo "Local port: $port busy"
+			else
+				echo "Local port: $port free"
 			fi
 		done	
 	done
